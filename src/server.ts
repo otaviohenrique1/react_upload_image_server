7f5a2422cceb670c08a685d51a348ec0af34bc7d
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-30 21:35:18 -0300

    commit 4 dos arquivos

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,7 @@ import 'express-async-errors';
 import "./connection";
 import routes from "./routes";
 import errorHandler from "./errors/handler";
-// import createError from "http-errors";
-// import morgan from "morgan";
-// import multer from "multer";
 import { PORT } from "./config/constantes";
-// import { MULTER_OPTIONS_DEST } from "./config/constantes";
-// const upload = multer({ dest: MULTER_OPTIONS_DEST })
 
 const app = express();
 
@@ -21,10 +16,3 @@ app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log("Express iniciado..."));
-
-/*
-  colocar no form para que o multer consiga processar -> enctype="multipart/form-data"
-  <form action="/profile" method="post" enctype="multipart/form-data">
-    <input type="file" name="avatar" />
-  </form>
-*/
